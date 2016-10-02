bac6a2bb9002931fca789911f06f404db1d68257
seba3c <sebastian.castaneda@devsar.com>
2016-10-02 19:51:27 -0300

    Minor fix

--- settings/heroku_settings.py
+++ settings/heroku_settings.py
@@ -1,4 +1,5 @@
 import os.path
+import dj_database_url
 
 from settings.base_settings import *
 
@@ -33,6 +34,10 @@ DATABASES = {
     #     'PORT': '5432',
     # }
 }
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 FTPD_ROOT = '/tmp/ftpd'
 FTPD_HOST = '0.0.0.0'
