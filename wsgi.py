46a822df16521fcc2b26ea6c3766f779f22246af
seba3c <castanedacs@gmail.com>
2017-04-17 20:51:02 -0300

    Deleted unused files

--- wsgi.py
+++ wsgi.py
@@ -10,6 +10,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.heroku_settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.prod_settings")
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
